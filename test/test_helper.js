fdf8c8c86bef9d90cb00219c227cd6210da094ef
Manu Uzkudun <uzkudun@mdpi.com>
2016-10-31 17:40:33 +0100

    renderComponent implemented in the testHelper

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
 import jquery from 'jquery';
 
@@ -9,6 +10,10 @@ global.window = global.document.defaultView;
 const $ = jquery(global.window);
 
 // Build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass) {
+  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
+}
 
 // Build helper for simulating events
 
